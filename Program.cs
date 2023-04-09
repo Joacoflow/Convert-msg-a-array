e5f66953bbe196f2206981bbed72b3fb37c7fb51
Joacoflow <joaquinalfredo6@gmail.com>
2023-04-09 06:39:48 -0500

    programa en c#

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3__ESCRIBA_UN_MENSAJE_Y_LO_DEVUELVO_COMO_ARRAY
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string mensaje;
+            Console.Write("Ingrese un mensaje: ");
+            mensaje=Console.ReadLine();
+
+            char[] caracter = mensaje.ToCharArray();
+
+            string texto = "c[" + caracter.Length + "] = ";
+
+            for (int i = 0; i < caracter.Length; i++)
+
+            {
+                texto += "[" + caracter[i] + "] ";
+
+            }
+
+            Console.WriteLine(texto);
+            Console.ReadKey();
+
+
+        }
+    }
+}
